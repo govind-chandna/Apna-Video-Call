2d6ea2985af210c79a76b0a442eac18c159730b5
govind gujjar <govindgujjar.in@gmail.com>
2026-07-02 22:58:25 +0530

    Update backend URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,7 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
 
-    "http://localhost:8000"
-
+const server = IS_PROD
+  ? "https://apna-video-call-7v4z.onrender.com"
+  : "http://localhost:8000";
 
 export default server;
