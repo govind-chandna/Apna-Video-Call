07c12408c62b0beecb825335ceef7748dabd5992
govind gujjar <govindgujjar.in@gmail.com>
2026-09-11 12:02:37 +0530

    Updated backend deployment URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-  ? "https://apna-video-call-7v4z.onrender.com"
+  ? "https://apna-video-call-tnh6.onrender.com"
   : "http://localhost:8000";
 
 export default server;
